097a7fa161d92fa2824e81ecf8fa5d30b843f389
BRIGHT OMOLAYO <molayobright@gmail.com>
2024-02-13 11:50:41 +0100

    ...

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const input = document.querySelector('input')
 const btn = document.querySelector('button')
+const imageParagraph = document.getElementById('imgp')
+const imageCon = document.getElementById('sub-image-container')
 
 btn.addEventListener('click', () => {
   fetch(`https://api.weatherapi.com/v1/current.json?key=69e6d554b0894e1fa4d84719241202&q=${input.value}`, { mode: 'cors' })
     .then(response => response.json())
 
     .then(json => {
-      document.getElementById('img').append(json.current.condition.text)
+      imageParagraph.append(json.current.condition.text)
       console.log(json)
     })
 })
@@ -18,3 +20,19 @@ document.getElementById('refreshbtn').addEventListener('click', function () {
   // eslint-disable-next-line no-undef
   location.reload()
 })
+
+// eslint-disable-next-line eqeqeq
+if ( ${imageParagraph.value} === 'Sunny'), {
+  imageCon.body.style.backgroundImage = "url('./images/sunny.jpg')"
+}
+
+/* const icon = {
+  sunny: 'C:\Users\HP\weatherapp\images\sunny.jpg'
+  clody : 'C:\Users\HP\weatherapp\images\cloudy.jpg'
+  foggy:'C:\Users\HP\weatherapp\images\foggy.jpg'
+  rainfall:'C:\Users\HP\weatherapp\images\rain-fall.jpg'
+  sleet:'C:\Users\HP\weatherapp\images\sleet.jpg'
+  snowy:'C:\Users\HP\weatherapp\images\snowy.jpg'
+  thunder:'C:\Users\HP\weatherapp\images\thunder.jpg'
+  windy:'C:\Users\HP\weatherapp\images\Windy.jpg'
+} */
